b519fd9d0218594830b40bf95a4d80f196c93f06
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2023-01-13 21:04:54 +0600

    trying to solve node modules ignore

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.send("Welcome to Laptop Mart Server");
+  res.send("Welcome to Laptop Mart Server!!!");
 });
 
 app.listen(port, () => {
