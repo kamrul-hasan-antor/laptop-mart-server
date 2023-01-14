55e3bf7ac2b5f31dd9453bc292465f3260819b32
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2023-01-14 12:55:27 +0600

    user posting

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7ky8m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("laptopMart").collection("users");
+
+    app.post("/addUsers", async (req, res) => {
+      const users = req.body;
+      // const result = await userCollection.insertOne(users)
+      console.log(users);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", async (req, res) => {
   res.send("Welcome to Laptop Mart Server!!!");
